ec084b65c1050259b4a8f9aa565fba4ebb7b2118
aashraf2510 <aashraf.elevatetech@gmail.com>
2025-04-10 23:33:26 +0300

    testqqq

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,2 @@
 import { Routes } from '@angular/router';
-import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout.component';
-import { HomeComponent } from './features/pages/home/home.component';
-import { authGuard } from './core/guards/auth/auth.guard';
-import { loggedUserGuard } from './core/guards/auth/logged-user.guard';
-
 export const routes: Routes = [];
