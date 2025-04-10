db5aaa157b6415c39ed1c89d607a6af031fd2a9b
aashraf2510 <aashraf.elevatetech@gmail.com>
2025-04-10 22:50:53 +0300

    tesr

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'home/exams/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
+  // {
+  //   path: 'home/exams/:id',
+  //   renderMode: RenderMode.Server,
+  // },
+  // {
+  //   path: '**',
+  //   renderMode: RenderMode.Prerender,
+  // },
 ];
