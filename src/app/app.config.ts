7e029f416ddf8ca9a9d3bd121f4b22f06490cca0
aashraf2510 <aashraf.elevatetech@gmail.com>
2025-04-10 23:38:52 +0300

    ww

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,34 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
-import { BASE_URL_TOKEN } from 'auth-api';
-import { environment } from './environments/environments';
-import { provideStore } from '@ngrx/store';
-import { authReducer } from './store/auth.reducer';
-import { headersInterceptor } from './core/interceptors/headers.interceptor';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(), withInterceptors([headersInterceptor])),
-    {
-      provide: BASE_URL_TOKEN,
-      useValue: environment.baseUrl,
-    },
-    provideStore({ auth: authReducer }),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
